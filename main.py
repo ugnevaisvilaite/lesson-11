fafde2dc120c1e0e4d450405e5e28e44c3bcb1a4
ugnevaisvilaite <ugnevais88@inbox.lt>
2019-04-08 19:30:46 +0300

    Add refactored game

---
+++ main.py
@@ -0,0 +1,13 @@
+from controller import printHighscores
+from controller import  playGame
+
+printHighscores()
+
+nuo = int(input("Nuo kurio skaiciaus spesim? Kokia intervalo pradzia?"))
+iki = int(input("Iki kurio skaiciaus spesim? Kokia intervalo pabaiga?"))
+
+playGame(nuo=nuo, iki=iki)
+
+# r - read
+# w - write (perrasys tai, kas viduje yra)
+# a - add write (dades iki to, kas viduje yra)
